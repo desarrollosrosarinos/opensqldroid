136ab97fc3c70efa3593d7f8d7b88a7f441f33c1
nico <nicolas@crayonhost.com.ar>
2019-06-18 20:35:03 -0300

    Initial tests

---
+++ app/src/main/java/ar/com/desarrollosrosarinos/opensqldroid/activities/ServerConnectionNew.java
@@ -0,0 +1,15 @@
+package ar.com.desarrollosrosarinos.opensqldroid.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import ar.com.desarrollosrosarinos.opensqldroid.R;
+
+public class ServerConnectionNew extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.server_connection_new);
+    }
+}
